ba1654889435900c1b8d6f241ab807b06a92001e
Mildnoodles <adamt@vt.edu>
2019-03-27 08:35:30 -0400

    Finished product

--- test/weather.test.js
+++ test/weather.test.js
@@ -11,8 +11,7 @@ const request = require('supertest');
   Good Zip, celcius Scale
   Good Zip, Fahrenheit Scale
   Same cases, but with a bad zip
-  Then additional malformed GET requests
- */ 
+*/ 
 
 //Good Zip
 describe("First Test", function() {
@@ -46,7 +45,8 @@ describe("Fourth Test", function() {
   it("Tests a good Zip code with scale as Fahrenheit", function(done) {
     request(weather).get("/locations/24060?scale=Fahrenheit")
         .expect(200)
-        .expect(/200 { "temperature": **, "scale": "Fahrenheit"} /, done)
+        .expect(/200/)
+        .expect(/,\n     "scale": "Fahrenheit"\n}/, done)
   })
 })
 
@@ -55,33 +55,34 @@ describe("Fifth Test", function() {
   it("Tests a good Zip code with scale as Celsius", function(done) {
     request(weather).get("/locations/24060?scale=Celsius")
         .expect(200)
-        .expect(/200 { "temperature": **, "scale": "Fahrenheit"} /, done)
+        .expect(/200/)
+        .expect(/,\n     "scale": "Celsius"\n}/, done)
   })
 })
 
 //Bad Zip, bad scale
 describe("Sixth Test", function() {
   it("Tests a bad Zip code and a bad scale", function(done) {
-    request(weather).get("/locations/24060")
-        .expect(200)
-        .expect(/200/, done)
+    request(weather).get("/locations/240?scale=c")
+        .expect(404)
+        .expect(/Invalid scale. Valid scales are "Fahrenheit" and "Celsius"/, done)
   })
 })
 
 //Bad Zip, scale Fahrenheit
-describe("First Test", function() {
-  it("Tests a good Zip code", function(done) {
-    request(weather).get("/locations/24060")
-        .expect(200)
-        .expect(/200/, done)
+describe("Seventh Test", function() {
+  it("Tests a bad Zip code with scale as Fahrenheit", function(done) {
+    request(weather).get("/locations/2406?scale=Fahrenheit")
+        .expect(404)
+        .expect(/The request resulted in an error: {"cod":"404","message":"city not found"}/, done)
   })
 })
 
 //Bad Zip, scale Celsius
-describe("First Test", function() {
-  it("Tests a good Zip code", function(done) {
-    request(weather).get("/locations/24060")
-        .expect(200)
-        .expect(/200/, done)
+describe("Eigth Test", function() {
+  it("Tests a bad Zip code with scale as Celsius", function(done) {
+    request(weather).get("/locations/240?scale=Celsius")
+        .expect(404)
+        .expect(/The request resulted in an error: {"cod":"404","message":"city not found"}/, done)
   })
 })
